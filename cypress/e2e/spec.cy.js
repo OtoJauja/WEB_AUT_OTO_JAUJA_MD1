d794e5a629ce7aa25b04d10d077b081d5f5db23e
OtoJauja <97533069+OtoJauja@users.noreply.github.com>
2023-06-10 18:29:59 +0300

    super

--- cypress/e2e/spec.cy.js
+++ cypress/e2e/spec.cy.js
@@ -1,30 +1,25 @@
+import SelectPage from "../pageObjects/SelectPage";
 
+describe('Select grid', () => {
+  beforeEach(() => {
+    SelectPage.visit();
+  });
 
-describe('Selectable Grid Test', () => {
-  it('should select specific numbers in the grid', () => {
-    cy.visit('https://demoqa.com/selectable');
+  it('Clicks and validates if highlited or not', () => {
+    // izveido masivu ar pogām kuras nospiedīs
+    const selected = ['Two', 'Four', 'Six', 'Eight'];
+    const unselected = ['One', 'Three', 'Five', 'Seven', 'Nine'];
+
+    // pasauc metodi kas aizved uz grid
+    SelectPage.selectGrid.click();
     
-    // Click "Grid" tab
-    cy.contains('Grid').click();
-    
-    // Click specific numbers - Two, Four, Six, Eight
-    cy.get('#gridContainer > :nth-child(2)').click();
-    cy.get('#gridContainer > :nth-child(4)').click();
-    cy.get('#gridContainer > :nth-child(6)').click();
-    cy.get('#gridContainer > :nth-child(8)').click();
-    
-    // Validate that selected numbers are highlighted
-    cy.get('.ui-selected').should('have.length', 4);
-    cy.contains('Two').should('have.class', 'ui-selected');
-    cy.contains('Four').should('have.class', 'ui-selected');
-    cy.contains('Six').should('have.class', 'ui-selected');
-    cy.contains('Eight').should('have.class', 'ui-selected');
-    
-    // Validate that unselected numbers are not highlighted
-    cy.get('.ui-selected').should('not.contain', 'One');
-    cy.get('.ui-selected').should('not.contain', 'Three');
-    cy.get('.ui-selected').should('not.contain', 'Five');
-    cy.get('.ui-selected').should('not.contain', 'Seven');
-    cy.get('.ui-selected').should('not.contain', 'Nine');
+    // pasauc metodi kas nospiež katru vajadzīgo pogu
+    SelectPage.selectNumber(selected);
+
+    // pasauc metodi kas validē kas ir highlited
+    SelectPage.validateSelected(selected);
+
+    // pasauc metodi kas validē kas nav highlited
+    SelectPage.validateUnselected(unselected);
   });
 });
